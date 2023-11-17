a2c9c01bcc7391c921069f824556824992de25ce
returnullptr <116039823+returnullptr@users.noreply.github.com>
2023-11-16 18:47:42 -0800

    *Grid Map In Progress*

--- Team-308-VirtualWarehouse/GridMap.cs
+++ Team-308-VirtualWarehouse/GridMap.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.drawing;
-using System
 namespace Team_308_VirtualWarehouse
 {
 	public class GridMap
@@ -9,10 +7,14 @@ namespace Team_308_VirtualWarehouse
         private const int gridSize = 5;
         private const int gridScale = 2; // Each grid is 2x2 feet
 
+        private Ellipse circle;
+
         // The data structure that will hold the real-world coordinates of the center of each grid.
         private (int x, int y)[,] gridContents;
+        // variable to set origin coordinatest
+        private (int x, int y) origin;
+        private (int x, int y) currentCoordinates;
 
-        private Ellipse circle;
 
         public GridMap()
         {
@@ -30,10 +32,9 @@ namespace Team_308_VirtualWarehouse
                     gridContents[i, j] = (realWorldX, realWorldY);
                 }
             }
-            InitializeComponent();
-        }
+        }  
 
-        // Method to set the content of a grid cell
+        // example set
         public void SetGridContent(int x, int y)
         {
             if (x < 0 || x >= width || y < 0 || y >= height)
@@ -43,7 +44,7 @@ namespace Team_308_VirtualWarehouse
             gridContents[i, j] = (x, y);
         }
 
-        // Method to get the content of a grid cell
+        // example get
         public GridContent GetGridContent(int x, int y)
         {
             if (x < 0 || x >= width || y < 0 || y >= height)
@@ -55,16 +56,45 @@ namespace Team_308_VirtualWarehouse
 
         public void SetCircle(int x, int y)
         {
-            if(circle != NULL){
+            if (circle != NULL)
+            {
                 this->circle = null;
                 Ellipse circle = new Ellipse(50, 50, x, y);
             }
-            else if (circleDraw == NULL){
+            else if (circleDraw == NULL)
+            {
                 Ellipse circle = new Ellipse(50, 50, x, y);
             }
         }
+
+        // overloading with parser
+        public GridContent GetCoordinates()
+        {
+            currentCoordinates = Form1.GetCoordinates();
+            Console.WriteLine($"X: {currentCoordinates.x}, Y: {currentCoordinates.y}");
+        }
+
+        // Function to set the origin
+        public void setOrigin()
+        {
+            // 'origin' holds the x, y, z values used for further calculations
+            origin = Form1.GetCoordinates();
+        }
+
+        public void DisplayOrigin()
+        {
+            Console.WriteLine($"Origin is set to: X={origin.x}, Y={origin.y}");
+        }
+
+        // get current location difference than original location
+        public (int x, int y) calculateLocationDifference()
+        {
+            int diffX = currentCoordinates.x - origin.x;
+            int diffY = currentCoordinates.y - origin.y;
+            return (diffX, diffY);
+        }
+
     }
 
 }
 
-    
